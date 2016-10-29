24189fe7c070be2e5e4b43a2a94462c6dd086dcd
ahale <andrew.hale@rackspace.co.uk>
2016-10-29 13:28:43 +0100

    add highstate cron

--- init.sh
+++ init.sh
@@ -4,12 +4,26 @@ yum install -y https://repo.saltstack.com/yum/amazon/salt-amzn-repo-latest-1.ami
 yum clean -y expire-cache
 yum update -y
 yum upgrade -y
-yum install -y salt-minion
+yum install -y salt-minion python26-pip.noarch
+pip-2.6 install 'GitPython<2.0.9'
+pip-2.6 install argparse
 
-echo "file_client: local" > /etc/salt/minion
+echo "
+file_client: local
+fileserver_backend:
+    - git
+
+gitfs_remotes:
+    - https://github.com/ahale/myznc.git:
+        - name: myznc_salt_states
+        - root: salt/states
+    - https://github.com/ahale/myznc.git:
+        - name: myznc_salt_files
+        - root: salt/files
+
+" > /etc/salt/minion
 
 chkconfig salt-minion off
 service salt-minion stop
 
-salt-call state.highstate
-
+salt-call --local -l debug  state.highstate
